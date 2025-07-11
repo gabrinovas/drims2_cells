1c258c28c2da244f27f049b8208e92040cb1d82d
Samuele Sandrini <samuele.sandrini@polito.it>
2025-07-11 10:18:19 +0200

    Added moveit server

---
+++ drims2_description/launch/ur5e/ur5e_moveit_server.launch.py
@@ -0,0 +1,83 @@
+
+# Copyright 2024 National Research Council STIIMA
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+from moveit_configs_utils import MoveItConfigsBuilder
+
+
+def get_moveit_configs():
+    srdf_path = os.path.join(get_package_share_directory('drims2_ur5e_moveit_config'), 'config', 'platform.srdf')
+    joint_limits_path = os.path.join(get_package_share_directory('drims2_ur5e_moveit_config'), 'config', 'joint_limits.yaml')
+    moveit_controllers_path = os.path.join(get_package_share_directory('drims2_ur5e_moveit_config'), 'config', 'moveit_controllers.yaml')
+    pilz_limits_path = os.path.join(get_package_share_directory('drims2_ur5e_moveit_config'), 'config', 'pilz_cartesian_limits.yaml')
+    robot_description_path = os.path.join(get_package_share_directory('drims2_description'), 'urdf', 'ur5e','ur5e_cell.urdf.xacro') 
+    robot_description_args = {
+      "use_fake_hardware" : 'True'
+    }
+
+    moveit_config = (
+        MoveItConfigsBuilder('manipulator', package_name='drims2_ur5e_moveit_config')
+        .robot_description(file_path=robot_description_path,
+                            mappings= robot_description_args)
+        .robot_description_semantic(file_path=srdf_path)
+        .planning_scene_monitor(publish_robot_description=False,
+                                publish_robot_description_semantic=True,
+                                publish_planning_scene=True)
+        .planning_pipelines(default_planning_pipeline='ompl', pipelines=['ompl', 'chomp', 'pilz_industrial_motion_planner'])
+        .pilz_cartesian_limits(file_path=pilz_limits_path)
+        .joint_limits(file_path=joint_limits_path)
+        .trajectory_execution(file_path=moveit_controllers_path)
+        .robot_description_kinematics()
+        .moveit_cpp(
+            file_path=get_package_share_directory("drims2_description")
+            + "/config/ur5e/moveit_config.yaml"
+        )
+
+        .to_moveit_configs()
+    )
+    return moveit_config.to_dict()
+
+def generate_launch_description():
+    pkg_dir = get_package_share_directory('drims2_motion_server')
+    drims2_description_pkg_dir = get_package_share_directory('drims2_description')
+    motion_server_config_path_cmd = DeclareLaunchArgument(
+        'motion_server_config_path',
+        default_value=drims2_description_pkg_dir + '/config/ur5e/motion_server_config.yaml',
+        description='Full path to the config file')
+
+    motion_server_node = Node(
+        package='drims2_motion_server',
+        executable='motion_server',
+        name='motion_server_node',
+        output='screen',
+        parameters=[
+            LaunchConfiguration('motion_server_config_path'),
+            get_moveit_configs()
+        ])
+    
+    # Create the launch description and populate
+    ld = LaunchDescription()
+
+    ld.add_action(motion_server_config_path_cmd)
+    ld.add_action(motion_server_node)
+    return ld

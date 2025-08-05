fe865ca679afb22fe0d8d88e6c26852b67149525
Cesare Tonola <c.tonola001@unibs.it>
2025-08-05 13:50:51 +0200

    ur10e

---
+++ drims2_description/launch/ur10e/robotiq_hande_urcap_adapter.launch.py
@@ -0,0 +1,36 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    robot_ip_launch_arg = DeclareLaunchArgument(
+        "robot_ip", default_value="192.168.125.121"
+    )
+    robot_port_launch_arg = DeclareLaunchArgument(
+        "robot_port", default_value="63352"
+    )
+
+    robotiq_2f_urcap_adapter_node = Node(
+            package='robotiq_2f_urcap_adapter',
+            namespace='robotiq_2f_urcap_adapter',
+            executable='robotiq_2f_adapter_node.py',
+            name='robotiq_hande_urcap_adapter',
+            parameters=[{
+                "robot_ip": LaunchConfiguration('robot_ip'),
+                "robot_port": LaunchConfiguration('robot_port'),
+                "max_gripper_width": 0.05,
+                "min_gripper_width": 0.0,
+                "max_gripper_speed": 0.15,
+                "min_gripper_speed": 0.01,
+                "max_gripper_force": 185.0,
+                "min_gripper_force": 20.0,
+            }]
+        )
+    return LaunchDescription([
+        robot_ip_launch_arg,
+        robot_port_launch_arg,
+        robotiq_2f_urcap_adapter_node
+    ])
---
+++ drims2_description/launch/ur10e/ur10e_moveit.launch.py
@@ -0,0 +1,60 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, Command
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.parameter_descriptions import ParameterValue
+from moveit_configs_utils import MoveItConfigsBuilder
+
+def generate_launch_description():
+    return LaunchDescription([
+        DeclareLaunchArgument(name="fake", default_value="true", description="Use fake hardware"),
+        OpaqueFunction(function=launch_setup)
+    ])
+
+def launch_setup(context):
+    srdf_path = PathJoinSubstitution([FindPackageShare("drims2_ur10e_moveit_config"),"config","platform.srdf"]).perform(context)
+    joint_limits_path = PathJoinSubstitution([FindPackageShare("drims2_ur10e_moveit_config"),"config","joint_limits.yaml"]).perform(context)
+    moveit_controllers_path = PathJoinSubstitution([FindPackageShare("drims2_ur10e_moveit_config"),"config","moveit_controllers.yaml"]).perform(context)
+
+    moveit_config = (
+        MoveItConfigsBuilder("manipulator", package_name="drims2_ur10e_moveit_config")
+        .robot_description_semantic(file_path=srdf_path)
+        .planning_scene_monitor(
+            publish_robot_description=True,
+            publish_robot_description_semantic=True,
+            publish_planning_scene=True
+        )
+        .planning_pipelines(default_planning_pipeline="ompl", pipelines=["ompl"])
+        .joint_limits(file_path=joint_limits_path)
+        .trajectory_execution(file_path=moveit_controllers_path)
+        .to_moveit_configs()
+    )
+
+    move_group_node = Node(
+        package="moveit_ros_move_group",
+        executable="move_group",
+        output="screen",
+        parameters=[moveit_config.to_dict()],
+    )
+
+    rviz_config = PathJoinSubstitution([FindPackageShare("drims2_description"),"config","ur10e","rviz_config.rviz"]).perform(context)
+
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        arguments=["--display-config", rviz_config],
+        parameters=[
+            moveit_config.robot_description,
+            moveit_config.robot_description_semantic,
+            moveit_config.planning_pipelines,
+            moveit_config.robot_description_kinematics,
+        ],
+        output="screen"
+    )
+
+    return [
+      rviz_node,
+      move_group_node
+    ]

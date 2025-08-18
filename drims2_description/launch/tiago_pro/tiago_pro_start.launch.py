d1a56659f1a6fa9896eff4d992522336ea42c5ad
Samuele Sandrini <samuele.sandrini@polito.it>
2025-08-18 16:22:54 +0200

    Fix tiago import

--- drims2_description/launch/tiago_pro/tiago_pro_start.launch.py
+++ drims2_description/launch/tiago_pro/tiago_pro_start.launch.py
@@ -1,7 +1,7 @@
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.substitutions import LaunchConfiguration
-from launch.conditions import IfCondition
+from launch.conditions import IfCondition, UnlessCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory

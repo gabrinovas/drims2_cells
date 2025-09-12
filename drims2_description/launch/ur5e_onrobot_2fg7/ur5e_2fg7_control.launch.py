515c8c4f1b9bc78e71f4aaf23580559dcf698167
gabrinovas <gabriel.novas@aimen.es>
2025-09-12 14:30:00 +0200

    onrobot_2fg7_first try

---
+++ drims2_description/launch/ur5e_onrobot_2fg7/ur5e_2fg7_control.launch.py
@@ -0,0 +1,99 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, OpaqueFunction, IncludeLaunchDescription
+from launch.substitutions import PathJoinSubstitution, Command, FindExecutable, LaunchConfiguration
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.parameter_descriptions import ParameterValue
+from launch_ros.actions import Node
+from launch.conditions import IfCondition, UnlessCondition
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+def launch_setup(context, *args, **kwargs):
+    robot_description_content = Command([
+        PathJoinSubstitution([FindExecutable(name='xacro')]),
+        " ", 
+        PathJoinSubstitution([
+            FindPackageShare("drims2_description"), 
+            "urdf", "ur5e", "ur5e_cell.urdf.xacro"
+        ]),
+        " ", 
+        "use_fake_hardware:=", LaunchConfiguration("fake"),
+        " ",
+        "robot_ip:=", LaunchConfiguration("robot_ip"),
+    ])
+    robot_description = {
+        'robot_description': ParameterValue(robot_description_content, value_type=str)
+    }
+
+    controllers_config = PathJoinSubstitution([FindPackageShare("drims2_description"),
+    "config","ur5e","controllers.yaml"])
+
+    controller_manager_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[controllers_config,robot_description],
+        output="screen"
+        )
+
+    joint_trajectory_controller = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_trajectory_controller", 
+                   "--controller-manager", "/controller_manager"],
+        output='screen',
+    )
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster",
+                   "--controller-manager","/controller_manager"],
+        output='screen',
+    )
+
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="screen",
+        parameters=[robot_description]
+    )
+
+    # Reemplaza el spawner del controlador Robotiq por el del OnRobot
+    onrobot_fake_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["onrobot_2fg7_gripper_controller", 
+                   "--controller-manager", "/controller_manager"],
+        output='screen',
+        condition=IfCondition(LaunchConfiguration("fake"))
+    )
+
+    # Reemplaza el launch del URCap de Robotiq con el driver del OnRobot para hardware real
+    onrobot_driver_launch = IncludeLaunchDescription(
+            launch_description_source = PythonLaunchDescriptionSource(
+                PathJoinSubstitution([FindPackageShare("onrobot_ros2_driver"), "launch", "onrobot_driver.launch.py"])
+            ),
+            condition=UnlessCondition(LaunchConfiguration('fake')),
+            launch_arguments={'robot_ip': LaunchConfiguration("robot_ip")}.items() # O los argumentos que necesite
+        )
+    
+    what_to_launch = [
+        controller_manager_node,
+        joint_state_broadcaster_spawner,
+        joint_trajectory_controller,
+        robot_state_publisher_node,
+        onrobot_fake_controller_spawner,
+        onrobot_driver_launch
+    ]
+
+    return what_to_launch
+
+def generate_launch_description():
+    launch_args = []
+    launch_args.append(DeclareLaunchArgument(name="fake", default_value="true", description="use fake hardware"))
+    launch_args.append(DeclareLaunchArgument(name="robot_ip", default_value="0.0.0.0", description="Robot ip"))
+
+    ld = LaunchDescription(launch_args+[OpaqueFunction(function=launch_setup)])
+
+    return ld
